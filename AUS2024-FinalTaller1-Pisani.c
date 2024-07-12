832f6af014cc766ee98a830c0a1358b8853c4b43
Esteban Pisani <estebandpisani@gmail.com>
2024-07-12 00:03:00 -0300

    Agrega validación de cantidad de dados a relanzar.

--- AUS2024-FinalTaller1-Pisani.c
+++ AUS2024-FinalTaller1-Pisani.c
@@ -1,5 +1,6 @@
-#include "functions.h"
+#include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 
 //Valores de puntajes de cada categoría
 #define ESCALERA_VALUE 35
@@ -11,6 +12,20 @@
 #define TURNOS_MAX 11
 #define DADOS_MAX 5
 
+//Funciones útiles
+int arrojarDado(){
+    int dadoRandom = rand() % (7);
+    return dadoRandom;
+}
+
+void mostrarDado(int dadoId, int dado){
+    printf("- Dado %d: %d \n", dadoId, dado);
+}
+
+void separador(){
+    printf("-------------------\n");
+}
+
 int main(){
     int categorias[11] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     int cantidadJugadores = 0;
@@ -18,9 +33,8 @@ int main(){
     int lanzamientosRestantes = LANZAMIENTOS_MAX;
     int cantidadARelanzar = 0;
     int dadosActuales[DADOS_MAX];
-
-    char jugador1[20] = "";
-    char jugador2[20] = "";
+    char jugador1[21] = "";
+    char jugador2[21] = "";
 
     printf("¡Hola!, bienvenido al juego de la Generala!\n");
     do{
@@ -43,27 +57,67 @@ int main(){
         }
     }
 
-    //TODO lógica turnos
+    //TODO lógica turnos (for doble)
     //LOOP
     //Si turno==1
     printf("A continuación %s tirará los 5 dados por primera vez.\n", jugador1);
-    arrojarDados(dadosActuales);
+    printf("Arrojando dados...\n");
+    for (int i = 0; i < DADOS_MAX; i++)
+    {
+        dadosActuales[i] = arrojarDado();
+        mostrarDado(i+1, dadosActuales[i]);
+    }
     lanzamientosRestantes--;
-
+    printf("Lanzamientos restantes: %d \n", lanzamientosRestantes);
     do{
        printf("Cuántos dados desea relanzar? (0 = ninguno)\n");
        scanf("%d", &cantidadARelanzar);
 
-       if(cantidadARelanzar != 0){
-        //Seleccionar dados
-        int dadosReemplazos[cantidadARelanzar];
-        seleccionarDados(cantidadARelanzar, dadosReemplazos);
-        //Reemplazar dados seleccionados
-        actualizarDados(dadosActuales, dadosReemplazos);
-        lanzamientosRestantes--;
-       }
+        if(cantidadARelanzar<0 || cantidadARelanzar>5){
+            printf("Ingrese una cantidad correcta!! (0 a 5)\n");
+            continue;
+        }
 
-    } while(lanzamientosRestantes>0 || cantidadARelanzar==0);
+        if(cantidadARelanzar != 0){
+        if(cantidadARelanzar ==5){
+            printf("Relanzando todos los dados...\n");
+            for (int i = 0; i < DADOS_MAX; i++){
+                dadosActuales[i] = arrojarDado();
+            }   
+        } else {
+            separador();
+            printf("SELECCIONAR DADOS\n");
+            separador();
+            //Seleccionar dados
+            int dadosReemplazos[cantidadARelanzar];
+            for(int i=0; i<cantidadARelanzar; i++){
+                printf("Ingrese el número de dado que quiere relanzar (%d/%d) \n", i+1, cantidadARelanzar);
+                scanf("%d", &dadosReemplazos[i]);
+            }
+            //Reemplazar dados seleccionados
+            printf("Relanzando dados...\n");
+            for (int i = 0; i < (sizeof(dadosReemplazos)/ sizeof(dadosReemplazos[0])); i++)
+            {
+                dadosActuales[dadosReemplazos[i]-1] = arrojarDado();
+            }
+        }
+        separador();
+        //Mostrar dados actualizados
+        for (int i = 0; i < DADOS_MAX; i++)
+        {
+            mostrarDado(i+1, dadosActuales[i]);
+        }
+        //Lanzamientos restantes:
+        lanzamientosRestantes--;
+        if(lanzamientosRestantes == 0){
+            printf("Ya no le quedan lanzamientos en este turno.\n");
+        } else {
+            printf("Lanzamientos restantes: %d \n", lanzamientosRestantes);
+        }
+       }
+    } while(lanzamientosRestantes>0 && cantidadARelanzar!=0);
+
+    printf("Fin.\n");
    
     //while turno<=11
     //Turno 1: 
